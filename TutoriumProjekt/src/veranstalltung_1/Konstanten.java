6666fede7d5694993549f4eabadfc7b6353df45c
NErzberger <36168374+NErzberger@users.noreply.github.com>
2022-01-27 16:43:53 +0100

    VorbereitungVL1

---
+++ TutoriumProjekt/src/veranstalltung_1/Konstanten.java
@@ -0,0 +1,10 @@
+package veranstalltung_1;
+
+public class Konstanten {
+	
+	public static void main(String[] args) {
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Math.E);
+		System.out.println(Math.PI);
+	}
+}
---
+++ TutoriumProjekt/src/veranstalltung_1/uebungen/Uebung1.java
@@ -0,0 +1,11 @@
+package veranstalltung_1.uebungen;
+
+public class Uebung1 {
+
+	public static void main(String[] args) {
+		System.out.println("Vorname \t Name \t\t Studiengang");
+		System.out.println("Hans \t\t Peter \t\t WWIBE121");
+		System.out.println("August \t\t Müller \t WWIBE221");
+		System.out.println("Carolin \t Schmid \t WWIDS120");
+	}
+}
