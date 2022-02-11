b07e807ad23a07679747de29b5df6777baf00184
NErzberger <36168374+NErzberger@users.noreply.github.com>
2022-02-11 11:57:21 +0100

    Vorbereitung für Veranstaltung 2

---
+++ TutoriumProjekt/src/veranstaltung_2/BoolscherAusdruck.java
@@ -0,0 +1,19 @@
+package veranstaltung_2;
+
+public class BoolscherAusdruck {
+
+	public static void main(String[] args) {
+		// Direkter Zahlenvergleich
+		if(5 == 5) {}
+		
+		// Vergleich von Variablen
+		int var1 = 5;
+		int var2 = 5;		
+		if(var1 == var2) {}
+		if(var1 == 5) {}
+		
+		// über Variablen des Typs boolean
+		boolean variableDieStimmt = true;
+		if(variableDieStimmt) {}
+	}
+}
---
+++ TutoriumProjekt/src/veranstaltung_2/SwitchCase.java
@@ -0,0 +1,25 @@
+package veranstaltung_2;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		String tag = "Montag";
+		switch(tag) {
+		case "Montag":
+			System.out.println("Ich bin ein " + tag + ".");
+		case "Dienstag":
+			System.out.println("Ich bin ein " + tag + ".");
+		case "Mittwoch":
+			System.out.println("Ich bin ein " + tag + ".");
+		case "Donnerstag":
+			System.out.println("Ich bin ein " + tag + ".");
+		case "Freitag":
+			System.out.println("Ich bin ein " + tag + ".");
+		case "Samstag":
+			System.out.println("Ich bin ein " + tag + ".");
+		case "Sonntag":
+			System.out.println("Ich bin ein " + tag + ".");
+		}
+	}
+
+}
