6811f728e4497ccf7a077ec76512e675d9356710
Jonas Doerfler <doerfler.jonas@gmx.de>
2022-02-22 19:09:49 +0100

    Aufgabe 2 und 3

--- TutoriumProjekt/src/veranstaltung_3/uebungen/Uebung_3.java
+++ TutoriumProjekt/src/veranstaltung_3/uebungen/Uebung_3.java
@@ -1,5 +1,16 @@
 package veranstaltung_3.uebungen;
 
 public class Uebung_3 {
+	
+	public static void main(String[] args) {
+		
+		Kuh k = new Kuh();
+		double milch = k.melken();
+		System.out.println(milch);
+		
+		k.fressen(16);
+		
+		Kuh.info();
+	}
 
 }
