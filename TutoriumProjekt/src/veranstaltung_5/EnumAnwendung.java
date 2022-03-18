6f30067bb3ff6540405c32edfede8d2ec8e57a45
NErzberger <36168374+NErzberger@users.noreply.github.com>
2022-03-18 14:40:34 +0100

    verstanstaltung5

---
+++ TutoriumProjekt/src/veranstaltung_5/EnumAnwendung.java
@@ -0,0 +1,9 @@
+package veranstaltung_5;
+
+public class EnumAnwendung {
+	
+	public static void main(String[] args) {
+		Car audi = Car.Audi;
+		System.out.println("Das kostet ein Audi "+ audi.getPrice());
+	}
+}
---
+++ TutoriumProjekt/src/veranstaltung_5/uebungen/KeineAhnungWasJetzt.java
@@ -0,0 +1,13 @@
+package veranstaltung_5.uebungen;
+
+public class KeineAhnungWasJetzt extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public KeineAhnungWasJetzt(String message) {
+		super(message);
+	}
+}
